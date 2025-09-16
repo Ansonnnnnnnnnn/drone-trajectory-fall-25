8db414d823590b6d7c53fe14d701fc61fe5ed4d6
Anjian <ansonchen927@outlook.com>
2025-09-16 15:29:27 -0400

    dataset specification

--- src/data_model.py
+++ src/data_model.py
@@ -6,7 +6,13 @@ class DatasetSpec:
     Data model for specifications of an image dataset.
     """
 
-    pass
+    def __init__(self, overlap, sidelap, height, scan_dimension_x, scan_dimension_y, exposure_time_ms):
+        self.overlap = overlap
+        self.sidelap = sidelap
+        self.height = height
+        self.scan_dimension_x = scan_dimension_x
+        self.scan_dimension_y = scan_dimension_y
+        self.exposure_time_ms = exposure_time_ms
 
 
 class Camera:
