a7dbec55332ff6de082479d555287cf0a98f7161
Anjian <ansonchen927@outlook.com>
2025-10-26 19:47:30 -0400

    Complete visualization and prepared for presentation

--- src/visualization.py
+++ src/visualization.py
@@ -1,10 +1,8 @@
 """Utility to visualize photo plans.
 """
-
+import numpy as np
 import typing as T
-
 import plotly.graph_objects as go
-
 from src.data_model import Waypoint
 
 
@@ -17,4 +15,38 @@ def plot_photo_plan(photo_plans: T.List[Waypoint]) -> go.Figure:
     Returns:
         Plotly figure object.
     """
-    raise NotImplementedError()
+    x = [wp.x for wp in photo_plans]
+    y = [wp.y for wp in photo_plans]
+    hover_text = [
+        f"x={wp.x}, y={wp.y}, z={wp.z}<br>speed={wp.speed}, yaw={wp.yaw}°"
+        for wp in photo_plans
+    ]
+
+    fig = go.Figure()
+
+    # Connect points with line
+    fig.add_trace(go.Scatter(
+        x=x, y=y,
+        mode='lines+markers+text',
+        text=[f"{i}" for i in range(len(photo_plans))],  # waypoint index
+        textposition="top right",
+        hovertext=hover_text,
+        hoverinfo="text",
+        line=dict(color='blue', width=2),
+        marker=dict(size=8, color='red'),
+        name="Drone Path"
+    ))
+
+    fig.update_layout(
+        title="Drone Waypoints (2D Plot)",
+        xaxis_title="X",
+        yaxis_title="Y",
+        showlegend=True,
+        width=800,
+        height=600,
+        template="plotly_white"
+    )
+
+    fig.update_yaxes(scaleanchor="x", scaleratio=1)  # Equal aspect ratio
+
+    return fig
